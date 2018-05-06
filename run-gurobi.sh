bf1d4c52fa9111a630d4c8f2c8a4f3eaac4e3c9b
Fabio Kubo <fabio.kubo@dextra-sw.com>
2018-05-06 20:40:32 -0300

    udpating script

--- run-gurobi.sh
+++ run-gurobi.sh
@@ -2,6 +2,10 @@ make clean
 make
 for file in testes/*
 do
-  echo "Executando arquivo: $file"  >> gurobi-results.txt
-  ./pmr -i "$file" -g -t 10 >> output.txt
+  echo "========================================================================"
+  echo "========================================================================"
+  echo "==****************Executando arquivo: $file **************************=="  >> gurobi-results.txt
+  echo "========================================================================"
+  echo "========================================================================"
+  ./pmr -i "$file" -g -t 10 >> gurobi-results.txt
 done
